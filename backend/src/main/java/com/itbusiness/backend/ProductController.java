5a474e0cb300339c5348ee2f3ddc6a4c448626e1
sandz26 <sandiepkloof@gmail.com>
2025-09-15 22:20:23 +0200

    backend

---
+++ backend/src/main/java/com/itbusiness/backend/ProductController.java
@@ -0,0 +1,20 @@
+package com.itbusiness.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:3000") // allow React app during dev
+public class ProductController {
+
+    @GetMapping
+    public List<String> getProducts() {
+        return List.of("Shoes", "Bags", "Hats");
+    }
+}
+
